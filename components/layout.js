4a8109e2a2e5c6c5e4458c9a2a82b22e8ca38b6a
mana <iseetheworld1997312@gmail.com>
2024-04-27 19:16:17 +1000

    insert layout

---
+++ components/layout.js
@@ -0,0 +1,10 @@
+import useSWR from 'swr'
+import Navbar from './navbar'
+import Footer from './footer'
+export default function Layout({children}) {
+    return(
+        <>
+            <Navbar />
+        </>
+    )
+}
