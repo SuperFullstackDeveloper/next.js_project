5fae5e483c4cd3c15ecde17bda8e8f2028e6aadc
mana <iseetheworld1997312@gmail.com>
2024-04-28 16:34:08 +1000

    use router.replace

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,11 @@
-export default function About(){
-    return <div>About</div>
+import { useRouter } from 'next/router'
+
+export default function About() {
+    const router = useRouter()
+
+    return (
+        <button type="button" onClick={() => router.replace('/post')}>
+            Click me
+        </button>
+    )
 }
